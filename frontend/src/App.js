f8cf3bd0597ff0849bd8535c42f9833e2b2b110c
jason.c.lin <jason.c.lin@aic.aceraeb.com>
2025-02-06 21:49:43 +0800

    Update deployment configuration for username.github.io

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Resume from './pages/Resume';
 import Portfolio from './pages/Portfolio';
