d0b13c92c805c3b6268e0547e02e8a56b32e7a99
sheep52031 <sheep52031@users.noreply.github.com>
2025-05-19 04:24:21 +0000

    deploy: ac90aace6fa5f7582478a85a59408fcac55b3a5b

--- _next/static/2faue5AR5AS4fM829pHVj/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fautobiography","\u002F[lang]\u002Fportfolio","\u002F[lang]\u002Fresume"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/7hS1j929ZDNrE9mTC2YnQ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fautobiography","\u002F[lang]\u002Fportfolio","\u002F[lang]\u002Fportfolio\u002F[id]","\u002F[lang]\u002Fresume"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
